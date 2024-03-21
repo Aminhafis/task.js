fbb3cadd8847d85fd50f5a1128d170117010b189
Amin hafis <aminhafis@Amins-MacBook-Air.local>
2024-03-21 11:58:12 +0530

    hello

--- demo.js
+++ demo.js
@@ -1,6 +1,19 @@
-var a=20;
-var b= 15;
+// var a=20;
+// var b= 15;
 
-let sum=a+b;
+// let sum=a+b;
 
-console.log(sum)
+// console.log(sum)
+
+
+try {
+
+console.log("hello");
+console.log(a);
+console.log("world");
+    
+} catch (error) {
+    console.log(error.message);
+    
+}
+console.log("hello world");
