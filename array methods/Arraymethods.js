ae1d0755d600ebd32765dc33707d2faf1c864ddd
Aminhafis <aminhafis2@gmail.com>
2024-08-12 16:25:56 +0530

    hello

---
+++ array methods/Arraymethods.js
@@ -0,0 +1,10 @@
+
+
+var arr = [1,2,3,4,5,6]
+
+for (let i= 0;i< arr.length;i++){
+    if (i % 2 == 0){
+        
+        console.log(arr[i])
+    }
+}
---
+++ array methods/Arraymethodseg.js
@@ -0,0 +1,12 @@
+
+var s = 'hello world'
+var ch = 'o'
+let a = []
+
+for (let i=0; i<s.length;i++){
+    if (s[i] != ch){
+        a.push(s[i])
+
+    }
+}
+        console.log(a.join(''))
